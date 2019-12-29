a67de81f341c8d70b8bdb9ead1f324c2c93e2833
jianghaihao <654249738@qq.com>
2019-12-29 23:46:20 +0800

    2019-12-29 23:45:59

---
+++ heima_ssm_service/src/main/java/com/itheima/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.dao.IUserDao;
+import com.itheima.ssm.domain.UserInfo;
+import com.itheima.ssm.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userService")
+@Transactional
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private IUserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo userInfo = null;
+        try {
+            userInfo = userDao.findByUsername(username);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //处理自己的用户对象封装成UserDetails
+        //  User user=new User(userInfo.getUsername(),"{noop}"+userInfo.getPassword(),getAuthority(userInfo.getRoles()));
+        User user = new User(userInfo.getUsername(), userInfo.getPassword(), null);
+        return user;
+    }
+}
